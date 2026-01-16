82e7e10481d7ab9fb59d68779236af772ba9547b
wilson <wilsonkmzhou@hotmail.com>
2026-01-16 00:03:47 -0800

    changes so python venv works

---
+++ PredictingHousePrices.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+rawHousingData = pd.read_csv("https://raw.githubusercontent.com/wilkmzhou/vancouverHousePricePrediction/refs/heads/main/synthetic_house_prices_20_years.csv")
+
+print(rawHousingData.head(n = 3))
