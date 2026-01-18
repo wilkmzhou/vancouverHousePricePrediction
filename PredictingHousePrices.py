6c686c21ffffcfe42db1e70f0e43d94b3b5fd71e
wilson <wilsonkmzhou@hotmail.com>
2026-01-18 14:50:41 -0800

    cleaned dataset from NA values for lasso regression, unimportant variables like year observation recorded and season

--- PredictingHousePrices.py
+++ PredictingHousePrices.py
@@ -2,5 +2,16 @@ import pandas as pd
 
 rawHousingData = pd.read_csv("https://raw.githubusercontent.com/wilkmzhou/vancouverHousePricePrediction/refs/heads/main/synthetic_house_prices_20_years.csv")
 
-
 print(rawHousingData.head(n = 3))
+print(len(rawHousingData))
+
+rawHousingData["is_Renovated"] = rawHousingData["Renovation Year"].isnull().astype(int)
+rawHousingData["adjusted_house_age"] = 2024 - rawHousingData[["Year Built", "Renovation Year"]].max(axis = 1)
+rawHousingData["Garage Type"] = rawHousingData["Garage Type"].fillna("No Garage")
+rawHousingData = rawHousingData.drop(columns =["Renovation Year", "Year", "Season"],axis = 1)
+
+housingData = rawHousingData.dropna()
+housingData = housingData.drop_duplicates()
+
+print(housingData.head(n = 3))
+print(len(housingData))
